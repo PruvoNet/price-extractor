4e8cc0a6f3d267f8cbceb5d8fb58c72f58c979ae
regevbr <regevbr@gmail.com>
2018-02-22 13:21:04 +0200

    handle cases where string doesnt include thousnads seperator

--- examples/examples.js
+++ examples/examples.js
@@ -2,5 +2,7 @@ const extractor = require("./../index").searchPriceAndCode;
 
 console.dir(extractor("99,01€"));
 console.dir(extractor("ARS 1,647.86"));
+console.dir(extractor("ARS 1647.86"));
 console.dir(extractor("1.958,43 NOK"));
+console.dir(extractor("1958,43 NOK"));
 console.dir(extractor("￥732.62"));
